cdc2133da6625722c4204aaa26007785d2477b6a
S3baastian <kusnierz.sebastian@gmail.com>
2016-01-31 15:12:05 +0100

    Strona glowna - view

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new HelloBundle\HelloBundle(),
             new EpopeeBundle\EpopeeBundle(),
+            new ProjektBundle\ProjektBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/ProjektBundle/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ProjektBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('ProjektBundle:Default:index.html.twig');
+    }
+
+    public function categoryAction()
+    {
+        return $this->render('ProjektBundle:Kategorie:category.html.twig');
+    }
+}
